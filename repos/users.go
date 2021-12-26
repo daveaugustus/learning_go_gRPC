200be559a5cd56ec991f0780e6b1c3cb866ed31d
Dave Augustus <davetweetlive@gmail.com>
2021-12-27 04:04:43 +0530

    Done Find query

--- repos/users.go
+++ repos/users.go
@@ -1,9 +1,17 @@
 package repos
 
-import "github.com/go-xorm/xorm"
+import (
+	"fmt"
+
+	"github.com/davetweetlive/learning_go_gRPC/types"
+	"github.com/go-xorm/xorm"
+)
 
 // UsersRepo - the users repo interface
 type UsersRepo interface {
+	Create(*types.User) error
+	FindById(int64) (*types.User, error)
+	FindByEmail(string) (*types.User, error)
 }
 
 // NewUsersRepo - returns a new user repo
@@ -13,4 +21,53 @@ func NewUsersRepo(db *xorm.Engine) UsersRepo {
 
 type usersRepo struct {
 	db *xorm.Engine
+}
+
+func (u usersRepo) Create(user *types.User) (err error) {
+	if err = types.Validate(user); err != nil {
+		return
+	}
+
+	if _, err = u.db.Insert(user); err != nil {
+		return
+	}
+
+	return
+}
+
+func (u usersRepo) FindById(id int64) (user *types.User, err error) {
+	if id <= 0 {
+		err = fmt.Errorf("valid positive ID is required to find a user")
+		return
+	}
+
+	user = new(types.User)
+	has, err := u.db.ID(id).Get(user)
+	if err != nil {
+		return
+	}
+	if !has {
+		err = fmt.Errorf("unable to find user")
+	}
+
+	return
+}
+
+func (u usersRepo) FindByEmail(email string) (user *types.User, err error) {
+	if len(email) == 0 {
+		err = fmt.Errorf("valid email is required to find a user")
+		return
+	}
+
+	user = new(types.User)
+	user.Email = email
+	has, err := u.db.Get(user)
+	if err != nil {
+		return
+	}
+	if !has {
+		err = fmt.Errorf("unable to find user")
+	}
+
+	return
 }
